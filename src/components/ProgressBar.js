51df35106779282ac9a2d0b65b042475eeca828b
Kunal Baran Maiti <kmaiti1@jhu.edu>
2019-11-08 21:17:31 -0500

    Fixing untracked files

---
+++ src/components/ProgressBar.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Filler from "../components/Filler"
+
+const ProgressBar = (props) => {
+    return (
+        <div className="progress-bar">
+            <Filler/> 
+        </div>
+    )
+}
+
+export default ProgressBar
